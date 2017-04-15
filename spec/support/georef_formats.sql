f0b43b3cfc7a327f67ca5d95fc4265db7027cd76
Frank Dean <frankdean@users.noreply.github.com>
2017-04-15 18:55:55 +0100

    Added Open Location Code option for itinerary waypoints

--- spec/support/georef_formats.sql
+++ spec/support/georef_formats.sql
@@ -21,23 +21,24 @@ COPY georef_format (key, value, ord) FROM stdin;
 %d°%c	D+	3
 %i%d	D	4
 %p%d	±D	5
-%c%D° %M	QLandkarte GT	6
+plus+code	OLC plus+code	6
 %dd%M'%S"%c	Proj4	7
-%c%d°%M′%S″	+DMS	8
-%c%d°%M′\\	+DM	9
-%c%d°	+D	10
-%d° %M′ %S″ %c	D M S +	11
-%d° %M′ %c	D M +	12
-%d° %c	D +	13
-%c %d° %M′ %S″	+ D M S	14
-%c %d° %M′	+ D M	15
-%c %d°	+ D	16
-%d %m %s%c	Plain DMS+	16
-%d %m%c	Plain DM+	17
-%d%c	Plain D+	18
-%c%d %m %s	Plain +DMS	19
-%c%d %m	Plain +DM	20
-%c%d	Plain +D	21
+%c%D° %M	QLandkarte GT	8
+%c%d°%M′%S″	+DMS	9
+%c%d°%M′\\	+DM	10
+%c%d°	+D	11
+%d° %M′ %S″ %c	D M S +	12
+%d° %M′ %c	D M +	13
+%d° %c	D +	14
+%c %d° %M′ %S″	+ D M S	15
+%c %d° %M′	+ D M	16
+%c %d°	+ D	17
+%d %m %s%c	Plain DMS+	17
+%d %m%c	Plain DM+	18
+%d%c	Plain D+	19
+%c%d %m %s	Plain +DMS	20
+%c%d %m	Plain +DM	21
+%c%d	Plain +D	22
 \.
 
 
