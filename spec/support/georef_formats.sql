e55a592472c042994d5bdc5330fab0ee22a12ee1
Frank Dean <frankdean@users.noreply.github.com>
2017-04-27 15:37:08 +0100

    Added British and Irish Grid options for itinerary waypoints

--- spec/support/georef_formats.sql
+++ spec/support/georef_formats.sql
@@ -22,23 +22,26 @@ COPY georef_format (key, value, ord) FROM stdin;
 %i%d	D	4
 %p%d	±D	5
 plus+code	OLC plus+code	6
-%dd%M'%S"%c	Proj4	7
-%c%D° %M	QLandkarte GT	8
-%c%d°%M′%S″	+DMS	9
-%c%d°%M′\\	+DM	10
-%c%d°	+D	11
-%d° %M′ %S″ %c	D M S +	12
-%d° %M′ %c	D M +	13
-%d° %c	D +	14
-%c %d° %M′ %S″	+ D M S	15
-%c %d° %M′	+ D M	16
-%c %d°	+ D	17
-%d %m %s%c	Plain DMS+	17
-%d %m%c	Plain DM+	18
-%d%c	Plain D+	19
-%c%d %m %s	Plain +DMS	20
-%c%d %m	Plain +DM	21
-%c%d	Plain +D	22
+osgb36	OS GB 1936 (BNG)	7
+%dd%M'%S"%c	Proj4	8
+%c%D° %M	QLandkarte GT	9
+%c%d°%M′%S″	+DMS	10
+%c%d°%M′\\	+DM	11
+%c%d°	+D	12
+%d° %M′ %S″ %c	D M S +	13
+%d° %M′ %c	D M +	14
+%d° %c	D +	15
+%c %d° %M′ %S″	+ D M S	16
+%c %d° %M′	+ D M	17
+%c %d°	+ D	18
+%d %m %s%c	Plain DMS+	18
+%d %m%c	Plain DM+	19
+%d%c	Plain D+	20
+%c%d %m %s	Plain +DMS	21
+%c%d %m	Plain +DM	22
+%c%d	Plain +D	23
+IrishGrid	Irish Grid	24
+ITM	Irish Transverse Mercator	25
 \.
 
 
